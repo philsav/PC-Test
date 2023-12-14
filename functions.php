96cc53cc00ec459fed26c20da386ce6a5bc75b4c
Philip S <philip.sav@gmail.com>
2023-12-14 11:13:39 -0500

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,30 @@
+// Add social profile links after the title in single.php using ACF
+
+function add_social_profile_links_after_title($title, $id) {
+    // Check if it's a single post
+    if (is_single() && in_the_loop() && $id === get_the_ID()) {
+        // Get custom fields for social profile links
+        $linkedin_profile = get_field('linkedin_profile', $id);
+        $facebook_profile = get_field('facebook_profile', $id);
+
+        // Output the social profile links after the title
+        if ($linkedin_profile || $facebook_profile) {
+            $title .= '<div class="social-profile-links">';
+            if ($linkedin_profile) {
+                $title .= '<a href="' . esc_url($linkedin_profile) . '" target="_blank" rel="nofollow" class="linkedin-profile-link">LinkedIn Profile</a>';
+            }
+            if ($linkedin_profile && $facebook_profile) {
+                $title .= ' | ';
+            }
+            if ($facebook_profile) {
+                $title .= '<a href="' . esc_url($facebook_profile) . '" target="_blank" rel="nofollow" class="facebook-profile-link">Facebook Profile</a>';
+            }
+            $title .= '</div>';
+        }
+    }
+
+    return $title;
+}
+
+// Hook the function to the_title hook
+add_filter('the_title', 'add_social_profile_links_after_title', 10, 2);
