1cbcc8d072b8739e72d93818365bcb62d9b2becf
Philip S <philip.sav@gmail.com>
2023-12-14 12:09:37 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 function add_social_profile_links_after_title($title, $id) {
     // Check if it's a single post
     if (is_single() && in_the_loop() && $id === get_the_ID()) {
+
         // Get custom fields for social profile links
         $linkedin_profile = get_field('linkedin_profile', $id);
         $facebook_profile = get_field('facebook_profile', $id);
@@ -11,13 +12,13 @@ function add_social_profile_links_after_title($title, $id) {
         if ($linkedin_profile || $facebook_profile) {
             $title .= '<div class="social-profile-links">';
             if ($linkedin_profile) {
-                $title .= '<a href="' . esc_url($linkedin_profile) . '" target="_blank" rel="nofollow" class="linkedin-profile-link">LinkedIn Profile</a>';
+                $title .= '<a href="' . esc_url($linkedin_profile) . '" target="_blank" rel="nofollow">LinkedIn</a>';
             }
             if ($linkedin_profile && $facebook_profile) {
                 $title .= ' | ';
             }
             if ($facebook_profile) {
-                $title .= '<a href="' . esc_url($facebook_profile) . '" target="_blank" rel="nofollow" class="facebook-profile-link">Facebook Profile</a>';
+                $title .= '<a href="' . esc_url($facebook_profile) . '" target="_blank" rel="nofollow">Facebook</a>';
             }
             $title .= '</div>';
         }
